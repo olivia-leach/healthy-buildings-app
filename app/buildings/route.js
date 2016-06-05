36fdb51acd595f0c6e166ab4367088494205c447
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-05 11:12:16 -0400

    Create building dashboard page
    List of user's buildings appear on building dashboard
    Clicking name of building brings you to individual building page

--- app/buildings/route.js
+++ app/buildings/route.js
@@ -6,13 +6,7 @@ export default Ember.Route.extend({
   // id: Ember.computed.alias('auth.credentials.id'),
 
   model() {
-    // return this.get('store').findAll('building');
-    // return this.get('store').query('building', {filter: {state: 'FL'}});
-    // return this.get('store').findRecord('building', 2);
-    // GET to /persons?filter[name]=Peter
-    this.store.query('building', { filter: { state: 'FL' } }).then(function(buildings) {
-      return buildings;
-    });
+    return this.get('store').findAll('building');
   },
 
 });
