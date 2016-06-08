83e40fe1437bb3b07b1c274b95121c59580e82ad
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-08 15:10:21 -0400

    Update building dashboard page
    With cards for each building
    Change delete button to x's

--- app/components/buildings-dashboard/component.js
+++ app/components/buildings-dashboard/component.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 const { $, get, set } = Ember;
 
+// $("#delete-button").click(function(e) {
+//   console.log('test');
+//    //do something
+//    e.stopPropagation();
+// });
+
 export default Ember.Component.extend({
   actions: {
   deleteBuilding() {
