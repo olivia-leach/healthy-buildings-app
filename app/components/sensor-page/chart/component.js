3725906e056bb2d3dd2452708ad4e8b74b0dd506
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-08 21:20:10 -0400

    Add charts to sensor page

--- app/components/sensor-page/chart/component.js
+++ app/components/sensor-page/chart/component.js
@@ -6,12 +6,12 @@ let color = d3.scale.category20();
 export default Ember.Component.extend({
 
   results: Ember.computed(function() {
-    let query = 'temp';
+    // let query = this.get('query');
     let results = [];
     let measures = this.get('points').toArray();
-    results.push(query);
-    for (let i = 0; i < measures.length; i++) {
-      results.push(measures[i].get(query));
+    results.push(this.get('query'));
+    for (let i = measures.length-100; i < measures.length; i++) {
+      results.push(measures[i].get(this.get('query')));
     };
     return results;
   }),
@@ -20,7 +20,7 @@ export default Ember.Component.extend({
     let results = [];
     let dates = this.get('points').toArray();
     results.push('x');
-    for (let i = 0; i < dates.length; i++) {
+    for (let i = dates.length-100; i < dates.length; i++) {
       results.push(dates[i].get('datetime'));
     }
     return results;
@@ -29,6 +29,34 @@ export default Ember.Component.extend({
   init: function() {
     this._super.apply(this, arguments);
     var self = this;
+    // console.log('test');
+    // console.log($('#drop-down option:selected').text());
+  },
+
+  updateChart: function() {
+    // console.log('testing');
+    let results = Ember.computed(function() {
+      let query = this.get('query');
+      let results = [];
+      let measures = this.get('points').toArray();
+      results.push(query);
+      for (let i = measure.length-50; i < measures.length; i++) {
+        results.push(measures[i].get(query));
+      };
+      return results;
+    });
+    console.log(results);
+    this.set('data', Ember.computed(function () {
+      return (
+        { x: 'x',
+          columns: [
+            this.get('dates'),
+            results
+          ],
+          // type: 'scatter'
+        })
+      }));
+    console.log(this.get('data'))
   },
 
   data: Ember.computed(function () {
@@ -36,107 +64,38 @@ export default Ember.Component.extend({
       { x: 'x',
         columns: [
           this.get('dates'),
-          //            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
           this.get('results')
-          // ['data1', 30, 200, 100, 400, 150, 250],
-          // ['data2', 130, 340, 200, 500, 250, 350]
-        ]
+        ],
+        color: this.get('color')
+        // type: 'scatter'
       })
     }),
 
-  axis: {
-    x: {
-      type: 'timeseries',
-      tick: {
-        format: '%Y-%m-%d'
-      }
+  axis: Ember.computed(function () {
+    let label = this.get('label');
+    return (
+      {
+        x: {
+          type: 'timeseries',
+          tick: {
+            fit: false
+            // format: '%YYYY-%m-%dd'
+          }
+        },
+        y: {
+                label: label
+            }
+    }
+  )}),
+
+  measures: ['temp', 'humidity', 'co2'],
+
+  actions: {
+    selectVehicle(query) {
+      console.log(query);
+      this.set('query', query);
+      this.updateChart();
     }
   }
 
-  // data: Ember.computed(function() {
-  //
-  //   let query = 'temp';
-  //   let result = [];
-  //   let measures = this.get('points').toArray();
-  //   for (let i = 0; i < measures.length; i++) {
-  //     result.push({
-  //       'time': d3.time.format('%Y-%m-%d').parse(measures[i].get('datetime')),
-  //       'label': query,
-  //       'value': measures[i].get(query),
-  //       // 'type': number
-  //     })
-  //   }
-  //   return result;
-  //   console.log(result);
-  // }),
-  //
-  // timeSeriesBarContent: [
-  //   {
-  //     time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
-  //     label: "Financial analytics software",
-  //     value: 49668,
-  //     type: "money"
-  //   }, {
-  //     time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
-  //     label: "Financial analytics software",
-  //     value: 68344,
-  //     type: "money"
-  //   }, {
-  //     time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
-  //     label: "Financial analytics software",
-  //     value: 60654,
-  //     type: "money"
-  //   }, {
-  //     time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
-  //     label: "Financial analytics software",
-  //     value: 48240,
-  //     type: "money"
-  //   }, {
-  //     time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
-  //     label: "Financial analytics software",
-  //     value: 62074,
-  //     type: "money"
-  //   }
-  // ],
-
-  // tagName: 'svg',
-  // attributeBindings: 'width height'.w(),
-  // content: Ember.computed(function() {
-  //   let measures = this.get('points').toArray();
-  //   let results = measures;
-  //   return results;
-  //   // console.log(results);
-  // }),
-  //
-  // transform: function(){
-  //   return 'translate(' + this.get('width')/2 + ',' + this.get('height')/2 + ')';
-  // },
-  //
-  // draw: function() {
-  //   let content = this.get('timeSeriesBarContent'),
-  //       width = this.get('width'),
-  //       height = this.get('height'),
-  //       radius = Math.min(width, height) / 2;
-  //
-  //   let arc = d3.svg.arc()
-  //       .outerRadius(radius)
-  //       .innerRadius(radius-15);
-  //
-  //   let pie = d3.layout.pie()
-  //       .sort(null)
-  //       .value(function(d) { return d.get('temp'); });
-  //
-  //   let svg = d3.select('#'+this.get('elementId')).select('g');
-  //
-  //   let g = svg.selectAll('.arc')
-  //      .data(pie(content)).enter()
-  //     .append('g')
-  //      .attr('class', 'arc');
-  //
-  //   g.append('path')
-  //     .attr('d', arc)
-  //     .attr('transform', "translate(" + this.get('width')/2 + "," + this.get('height')/2 + ")")
-  //     .attr("fill", function(d,i){ return color(i); })
-  //     .attr('class', function(d, i) { return d.data.get('id'); });
-  // }.on('didInsertElement')
 });
--- app/components/sensor-page/component.js
+++ app/components/sensor-page/component.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
+const { $, get, set } = Ember;
 
 export default Ember.Component.extend({
+  colors: d3.scale.category10(),
+  measures: [
+    { name: 'temp',
+      label: 'Temperature (C)',
+      color: "#1f77b4"},
+    { name: 'humidity',
+      label: 'Humidity (%)',
+      color: "#ff7f0e"},
+    { name: 'noise',
+      label: 'Noise (db)',
+      color: "#ff7f0e"},
+      { name: 'pressure',
+        label: 'Pressure (bars)',
+        color: "#ff7f0e"},
+    { name: 'co2',
+      label: 'Carbon Dioxide',
+      color: d3.scale.category10(0)},
+    ],
+  query: 'temp',
+  actions: {
+    selectVehicle(query) {
+      console.log(query);
+      this.set('query', query);
+    }
+  }
 });
