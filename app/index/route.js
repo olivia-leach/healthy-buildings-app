11d3df92fd1428d7e15b59a5ee0382dc92b5c3bd
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-09 19:47:54 -0400

    Update navbar styling

--- app/index/route.js
+++ app/index/route.js
@@ -4,12 +4,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   activate: function(){
+        // $('.navbar a').css('color', 'white');
         $('body').css('background', "url('images/backsplash.png') no-repeat center center fixed");
         $('body').css('background-size', 'cover');
+        // $('.nav > li > a:hover').css('color', 'white');
         Ember.$('body').append()
       },
   deactivate: function(){
         $('body').css('background', "none");
+        // $('.navbar a').css('color', 'black');
         // $('body').css('background', "url('images/ps_neutral.png')");
       }
 });
