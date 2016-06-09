24ef0f3ba99645e310eb889a740388648dada9a0
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-09 09:59:39 -0400

    Add patch for editing building name

--- app/building/route.js
+++ app/building/route.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('building', params.id);
   },
+
+  actions: {
+    updateBuilding (data) {
+      console.log('updating building...');
+      console.log(data.id);
+      this.get('store').findRecord('building', data.id).then(function(building) {
+        // console.log(building.name);
+        building.name = data.name;
+        return building.save();
+      });
+    }
+  }
 });
--- app/components/building-page/component.js
+++ app/components/building-page/component.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isEditing: false,
+  label: Ember.computed(function() {
+    return this.get('building.name')
+  }),
+  building: {},
+  save: function() {
+    this.set('isEditing', false);
+    console.log('saved');
+    this.set('building.name', this.get('label'));
+    this.set('building.id', this.get('building.id'));
+    this.sendAction('updateBuilding', this.get('building'));
+  },
+  edit: function() {
+    this.set('isEditing', true);
+  },
+
   baselineScore: Ember.computed(function() {
     let measures = this.get('building.measures');
     let baseline = 0;
@@ -19,5 +35,12 @@ export default Ember.Component.extend({
           return baseline;
         });
     }
-  })
+  }),
+
+  // actions: {
+  //   updateBuilding () {
+  //     console.log('updating building');
+  //     // this.sendAction('updateBuilding', this.get('building'));
+  //   }
+  // }
 });
