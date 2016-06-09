318fafafa583886652e81a37117be8ad96579d46
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-09 15:14:51 -0400

    try loading background image form component

--- app/index/route.js
+++ app/index/route.js
@@ -4,11 +4,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   activate: function(){
-        Ember.$('body').toggleClass("landing");
+        // Ember.$('body').toggleClass("landing");
+        $('body').css('background', "url('images/backsplash.png') no-repeat center center fixed");
+        $('body').css('background-size', 'cover');
+        // $('#clip').css('background', "url('images/backsplash.png') no-repeat center center fixed");
+        // $('#clip').css('background-size', 'cover');
+        // $('#clip').css("background-attachment", "fixed");
+        // $('#clip').css("-webkit-text-fill-color", "transparent");
+        // $('#clip').css("-webkit-background-clip", "text");
+        // $('#clip').css("font-size", "6em");
+        // $('#clip').css("font-family", "'Source Sans Pro', sans-serif");
+        // $('#clip').css("font-weight", "600");
+        // $('#clip').css("text-align", "center");
+        // $('#clip').css("padding", "10px 0");
+        // $('#clip').css("line-height", "0.9em");
+        // $('#clip').css("letter-spacing", "5px");
         Ember.$('body').append()
       },
   deactivate: function(){
-        Ember.$('body').toggleClass("landing");
+        // Ember.$('body').toggleClass("landing");
+        $('body').css('background-image', "none");
         Ember.$('body').toggleClass("rest");
       }
 });
